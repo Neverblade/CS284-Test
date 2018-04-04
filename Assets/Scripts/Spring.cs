ac1a207298afbaf6d9df14be3c0ca43303340d8e
James Lin <james97lin@gmail.com>
2018-04-03 21:14:01 -0700

    Added files.

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A spring. Connects two gameobjects (spheres).
+ * Attaches to the object referenced by objA.
+ */
+public class Spring : MonoBehaviour {
+
+    public GameObject objA;
+    public GameObject objB;
+    public float restLength;
+    public float ks;
+
+    void Start() {
+        restLength = (objA.transform.position - objB.transform.position).magnitude;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 diffAToB = objB.transform.position - objA.transform.position;
+        Vector3 dirAToB = diffAToB.normalized;
+        float displacement = diffAToB.magnitude - restLength;
+        objA.GetComponent<Rigidbody>().AddForce( dirAToB * displacement * ks, ForceMode.Force);
+        objB.GetComponent<Rigidbody>().AddForce(-dirAToB * displacement * ks, ForceMode.Force);
+	}
+}
